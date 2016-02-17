fef2b850a76740205bf9e0aa01eef796a9aad26b
peck2703 <peckmr@gmatc.matc.edu>
2016-02-17 13:35:01 -0600

    Initial Commit

---
+++ Assets/DamageCalculatiorInspector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class DamageCalculatiorInspector : MonoBehaviour {
+#if UNITY_EDITOR
+    [MenuItem ("GameObect/Create HelloWorld")] 
+
+    private static void CreateHelloWorldGameObject()
+    {
+        if (EditorUtility.DisplayDialog("Hello World","Do you really want to do this?","Create","Cancel"))
+        {
+            new GameObject("Hello World");
+        }
+    }
+#endif
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
